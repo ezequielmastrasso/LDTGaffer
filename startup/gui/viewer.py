55e22b5cdee71a6c924a5932ee6eb47c3a175525
ezequiel <ezequielm@gmail.com>
2020-09-05 22:25:49 -0400

    SceneView Phong Shader

--- startup/gui/viewer.py
+++ startup/gui/viewer.py
@@ -4,6 +4,7 @@ import GafferImage
 import GafferScene
 import GafferSceneUI
 import functools
+import inspect
 
 print ("LDTGAFFER:startup:gui:viewer")
 
@@ -48,3 +49,53 @@ def __createLDTPatternShader4k() :
     return pattern
 
 GafferSceneUI.SceneView.registerShadingMode( "LDTGafferPattern4k", functools.partial( __createLDTPatternShader4k ) )
+
+
+def __createPhongShader() :
+    phong = GafferScene.SceneProcessor( "Phong" )
+    phong["attributes"] = GafferScene.CustomAttributes()
+    phong["attributes"]["in"].setInput( phong["in"] )
+    phong["assignment"] = GafferScene.ShaderAssignment()
+    phong["assignment"]["in"].setInput( phong["attributes"]["out"] )
+    phong["shader"] = GafferScene.OpenGLShader( "phong" )
+    phong["shader"]["name"].setValue( "phong" )
+    phong["shader"]["type"].setValue( "gl:surface" )
+    phong["shader"]["parameters"].addChild( Gaffer.StringPlug( "glFragmentSource", defaultValue = inspect.cleandoc(
+        '''
+        in vec3 fragmentN;
+        in vec3 geometryP;
+        vec3 ADSLightModel(in vec3 normal, in vec3 pos){
+            vec3 lightPos = vec3(1.0, 0.5, 0.0);
+            vec3 lightAmbient = vec3(0.3, 0.3, 0.3);
+            vec3 lightDiffuse = vec3(1.0, 1.0, 1.0);
+            vec3 lightSpecular = vec3(1.0, 1.0, 1.0);
+            vec3 materialAmbient = vec3(0.9, 0.9, 0.9);
+            vec3 materialDiffuse = vec3(0.5, 0.5, 0.6);
+            vec3 materialSpecular = vec3(0.6, 0.6, 0.6);
+            float materialShininess = 100.0;
+            vec3 norm = normalize(normal);
+            vec3 lightv = normalize(lightPos - pos);
+            vec3 viewv = normalize(vec3(0.0) - pos);
+            vec3 refl = reflect(vec3(0.0) - lightv, norm);
+            vec3 ambient = materialAmbient * lightAmbient;
+            vec3 diffuse = max(0.0, dot(lightv, norm)) * materialDiffuse * lightDiffuse;
+            vec3 specular = vec3(0.0);
+            if(dot(lightv, viewv) > 0.0)
+            {
+                specular = pow(max(0.0, dot(viewv, refl)), materialShininess)*materialSpecular*lightSpecular;
+            }
+            return clamp(ambient + diffuse + specular, 0.0, 1.0);
+        }
+        void main(){
+            vec3 colour = ADSLightModel(fragmentN, geometryP);
+            gl_FragColor = vec4(colour, 1.0);
+        }
+    '''
+    ) ) )
+    phong["shader"]["out"] = Gaffer.Plug()
+    phong["assignment"]["shader"].setInput( phong["shader"]["out"] )
+    phong["out"].setInput( phong["assignment"]["out"] )
+
+    return phong
+
+GafferSceneUI.SceneView.registerShadingMode( "Phong", functools.partial( __createPhongShader ) )
